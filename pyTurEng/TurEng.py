69c01827644d2028e2d916780a5873104fd327af
Metin <metin@Metins-MacBook-Pro.local>
2016-01-19 02:25:48 +0200

    v0.5.0119160225

--- pyTurEng/TurEng.py
+++ pyTurEng/TurEng.py
@@ -1,33 +1,79 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+from time import sleep
 
 class TurEng(object):
-    def __init__(self):
-        self.url = "http://tureng.com/tr/turkce-ingilizce/"
+	def __init__(self):
+		self.url = "http://tureng.com/tr/turkce-ingilizce/"
 
-    def get_meaning(self,word,lang_class):
+	def get_meaning(self,word,lang_class):
 
-        request = requests.get(self.url+str(word))
-        content = request.content
-        soup = BeautifulSoup(content, "html.parser")
-        meaning_types = soup.findAll("td",{"class" : re.compile(r'\bhidden-xs\b')})
-        meanings = soup.findAll("td", {"class" : lang_class})
-        if len(meanings)>=5:
-            return meaning_types[1:15:3],meanings[:5]
-        else:
-            if meanings != None and meaning_types != None:
-                return meaning_types,meanings
+		request = requests.get(self.url+str(word))
+		content = request.content
+		soup = BeautifulSoup(content, "html.parser")
+		meaning_types = soup.findAll("td",{"class" : re.compile(r'\bhidden-xs\b')})
+		meanings = soup.findAll("td", {"class" : lang_class})
+		if len(meanings)>=5:
+			return meaning_types[1:15:3],meanings[:5]
+		else:
+			if meanings != None and meaning_types != None:
+				return meaning_types,meanings
+			else:
+				return None,None
 
 
-    def change_url(self,dic):
-        if dic == "tren":
-            self.url = "http://tureng.com/tr/turkce-ingilizce/"
-        elif dic == "deen":
-            self.url = "http://tureng.com/en/german-english/"
-        elif dic == "esen":
-            self.url = "http://tureng.com/en/spanish-english/"
-        elif dic == "fren":
-            self.url = "http://tureng.com/en/french-english/"
-            pass
+	def change_url(self,dic):
+		if dic == "tren":
+			self.url = "http://tureng.com/tr/turkce-ingilizce/"
+		elif dic == "deen":
+			self.url = "http://tureng.com/en/german-english/"
+		elif dic == "esen":
+			self.url = "http://tureng.com/en/spanish-english/"
+		elif dic == "fren":
+			self.url = "http://tureng.com/en/french-english/"
+			pass
+
+	def tag_to_str(self,r_list):
+		res = []
+		try:
+			for r in r_list:
+				res.append(r.text)
+			return res
+		except:
+			print("Error : {}".format(e))
+			exit()
+
+	def remove_duplicates(self,m_list):
+		new_list = []
+		for m in m_list:
+			if m not in new_list:
+				new_list.append(m)
+		return new_list
+
+	def search_from_file(self,input_file,output_file,lang_class):
+		try:
+			with open(input_file,"r") as input_f:
+				input_lines = input_f.readlines()
+		except Exception as e:
+			print("Error : {}".format(e))
+			exit()
+		try:
+			out_f = open(output_file,"a")
+			for line in input_lines:
+				r_list = self.get_meaning(line.split("\n")[0],lang_class)[1]
+				if len(r_list) == 1:
+					res = r_list.text
+				elif len(r_list) > 1:
+					res = ", ".join(self.remove_duplicates(self.tag_to_str(r_list)))
+				elif r_list == None:
+					res = " "
+				else:
+					res = " "
+				out_f.write(line.split("\n")[0] + " = " + res + "\n")
+			out_f.close()
+			print(str(len(input_lines)) + " word(s) were searched.")
+		except Exception as e:
+			print("Error : {}".format(e))
+			exit()
+		return True
--- pyTurEng/pyTurEng.py
+++ pyTurEng/pyTurEng.py
@@ -10,29 +10,42 @@ import os
 
 args = sys.argv
 
-if len(args) != 4:
-    print("Use as :\n'python pyTurEng.py tren tr merhaba\nor\n"
-          "python pyTurEng.py tren en \"go away\"\nor\n"
-          "python pyTurEng.py tren en wordlist.txt")
+if len(args)==4:
+	dic = args[1]
+	lang = args[2]
+	query = args[3]
+	dic_obj = TurEng()
+	dic_obj.change_url(dic)
+	if lang == "en":
+		result = dic_obj.get_meaning(query,"tr ts")
+	else:
+		result = dic_obj.get_meaning(query,"en tm")
+	types, meaning = result[0],result[1]
+	if len(meaning)==5:
+		for i in range(5):
+			print("{} : {}".format(types[i].text,meaning[i].text))
+	else:
+		if len(meaning)==0:
+			print("No Result")
+		else:
+			print("{} : {}".format(types[1].text,meaning[0].text))
+elif len(args)==5:
+	dic = args[1]
+	lang = args[2]
+	input_file = args[3]
+	output_file = args[4]
+	try:
+		dic_obj = TurEng()
+		dic_obj.change_url(dic)
+		if os.path.exists(input_file):
+			if lang == "en":
+				dic_obj.search_from_file(input_file,output_file,"tr ts")
+			else:
+				dic_obj.search_from_file(input_file,output_file,"en tm")
+	except Exception as e:
+		print("Error : {}".format(e))
+		exit()
 else:
-    dic = args[1]
-    lang = args[2]
-    query = args[3]
-    if os.path.exists(query):
-        pass
-    else:
-        dic_obj = TurEng()
-        dic_obj.change_url(dic)
-        if lang == "en":
-            result = dic_obj.get_meaning(query,"tr ts")
-        else:
-            result = dic_obj.get_meaning(query,"en tm")
-        types, meaning = result[0],result[1]
-        if len(meaning)==5:
-            for i in range(5):
-                print("{} : {}".format(types[i].text,meaning[i].text))
-        else:
-            if len(meaning)==0:
-                print("No Result")
-            else:
-                print("{} : {}".format(types[1].text,meaning[0].text))
+	print("Use as :\n'python pyTurEng.py tren tr merhaba\nor\n"
+		  "python pyTurEng.py tren en \"go away\"\nor\n"
+		  "python pyTurEng.py tren en wordlist.txt")
